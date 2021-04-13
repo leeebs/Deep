d0349389fa88d4152b280a1512a631946009205b
leeebs <66204538+leeebs@users.noreply.github.com>
2021-04-13 20:42:19 +0900

    Update train.py

--- CNN/train.py
+++ CNN/train.py
@@ -1,27 +1,14 @@
 import tensorflow as tf
 import argparse
-
 from dataset import Dataset
 from model import MyModel, VggModel, MiniInception, ResNet
 from loss import loss_fn
-#from evaluate import Test
-    #굳이 이런 식으로는 안하겠지
-    #하려면 parse를 여기서 바로 선언하겠지
-    #굳이 모듈화를 시킨 이유가 있을텐데 
-    #왜 argument를 따로 파일로 만들었을까 -> 다 함수로 처리
-    #필요한 parse만 그때그때 선언해서 만들기
 
 class Train(object):
     def __init__(self):
-        
-        #pre-trained, tfrecord, pytorch->tensorflow, custom model
-        # why 여기에 argparse를 넣냐 굳이 넣어야 할 이유가 있나 없으면 여기 넣으면 안되는 이유를 찾아
         self.save_path = args.save_model_dir
         self.train_ds = Dataset().split_ds_train()
         self.model = MyModel()
-            #self.model = VggModel()
-            #self.model = MiniInception()
-            #self.model = ResNet()
         self.optimizer = tf.keras.optimizers.Adam(args.learning_rate)
         self.train_loss = tf.keras.metrics.Mean()
         self.train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy()
@@ -42,8 +29,7 @@ class Train(object):
         self.train_accuracy(label, logits)
         
         return loss
-    
-    #train은 간단하게 잡다한 것은 main으로
+
     def main(self):
         
         self.train_loss.reset_states()
@@ -77,7 +63,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
     
     Train().main()
-    #Test().testing()
+
     
                 
             
