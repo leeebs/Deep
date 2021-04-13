178dc7fd5d00b4e54b73a660aa96f08bb1b9232c
leeebs <66204538+leeebs@users.noreply.github.com>
2021-04-13 20:44:59 +0900

    Add files via upload

---
+++ CNN/loss.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+
+from tensorflow import keras
+#from tensorflow.keras import layers
+
+
+def loss_fn(label, logits):
+    loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+    loss = loss(label, logits)
+    return loss
