895d033b3ac256eeafefd1f5f83e441a460d7ae4
leeebs <66204538+leeebs@users.noreply.github.com>
2021-07-04 18:44:01 +0900

    Update configuration.py

--- CNN_2/configuration.py
+++ CNN_2/configuration.py
@@ -12,10 +12,10 @@ test_image_dir = ""
 dataset_dir = "tfrecord\\dataset\\"
 train_dir = dataset_dir + "train"
 valid_dir = dataset_dir + "valid"
-test_dir = dataset_dir + "test"
+test_dir = dataset_dir + "test" 
 train_tfrecord = dataset_dir + "train.tfrecord"
 valid_tfrecord = dataset_dir + "valid.tfrecord"
-test_tfrecord = dataset_dir + "test.tfrecord"
+test_tfrecord = dataset_dir + "test.tfrecord" 
 
 TRAIN_SET_RATIO = 0.6
 TEST_SET_RATIO = 0.2
