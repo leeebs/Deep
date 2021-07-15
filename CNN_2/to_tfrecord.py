ffbcf21dd0344265ee552e09037e08680e2bea54
leeebs <66204538+leeebs@users.noreply.github.com>
2021-07-15 23:44:51 +0900

    Update to_tfrecord.py

--- CNN_2/to_tfrecord.py
+++ CNN_2/to_tfrecord.py
@@ -9,10 +9,10 @@ def bytes_feature(value):
     return tf.train.Feature(bytes_list = tf.train.BytesList(value=[value]))
 
 def float_feature(value):
-    return tf.train.Feature(float_list = tf.train.FloatList(value=[value]))
+    return tf.train.Feature(float_list = tf.train.FloatList(value=[value])) 
 
 def int64_feature(value):
-    return tf.train.Feature(int64_list = tf.train.Int64List(value=[value]))
+    return tf.train.Feature(int64_list = tf.train.Int64List(value=[value])) 
 
 def image_example(image_string, label):
     feature = {
